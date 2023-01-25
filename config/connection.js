d1a1ddd8cc12db54b5ec712c15dbbc3bac784b58
Bailey Mejia <baileymejia28@gmail.com>
2023-01-24 19:14:09 -0800

    Created config file, and created a models folder with the schema in there.

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const { connect, connection } = require('mongoose');
+
+//connect to mongoDB with mongoose
+//TODO insert route into connect method to make server run
+mongoose.connect('');
+
+module.exports = connection;
---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            ],
+        },
+    }
+)
+
